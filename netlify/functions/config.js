166d17e727e89707ae26152151e7f579dd2a3453
adaoulasoet <adaoulas.oet@gmail.com>
2026-07-06 18:05:57 +0200

    Update config.js

--- netlify/functions/config.js
+++ netlify/functions/config.js
@@ -11,7 +11,8 @@ exports.handler = async function(event, context) {
       projectId:         process.env.FIREBASE_PROJECT_ID,
       storageBucket:     process.env.FIREBASE_STORAGE_BUCKET,
       messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-      appId:             process.env.FIREBASE_APP_ID
+      appId:             process.env.FIREBASE_APP_ID,
+      netatmoClientId:   process.env.NETATMO_CLIENT_ID
     })
   };
 };
